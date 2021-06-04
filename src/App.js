1e44aa1debc8ac8ca2b158e42ff6fdddd1a4ab3f
Javier Ponce <javier.ponce@creditas.com>
2021-06-03 20:23:35 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Box from "@material-ui/core/Box";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box color="text.primary">
+      <h4>Nombre</h4>
+      <p>Pedro</p>
+    </Box>
   );
 }
 
