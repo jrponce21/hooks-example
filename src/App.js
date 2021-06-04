f972acf3b88f9529ec0ecb1ac333106970a66325
Javier Ponce <javier.ponce@creditas.com>
2021-06-04 12:55:40 -0500

    same flow with hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
+import React, { useState, useContext, useEffect } from "react";
 import "./App.css";
 import Box from "@material-ui/core/Box";
 import TextField from "@material-ui/core/TextField";
+import { ThemeContext } from "./context";
+
+function useDocumentWidth() {
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleWidth() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleWidth);
+
+    return () => window.removeEventListener("resize", handleWidth);
+  });
+  return width;
+}
+
+function useDocumentTitle(title) {
+  useEffect(() => {
+    document.title = title;
+  });
+}
+
+function useFormTextField(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleValueChange(e) {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleValueChange,
+  };
+}
 
 function App() {
+  const name = useFormTextField("Pedro");
+  const lastName = useFormTextField("Picapiedras");
+  const theme = useContext(ThemeContext);
+  const width = useDocumentWidth();
+
+  useDocumentTitle(name.value + " " + lastName.value);
+
   return (
-    <Box color="text.primary">
+    <Box color="text.primary" className={theme}>
       <h4>Nombre</h4>
-      <p>Pedro</p>
+      <TextField {...name} />
+      <h4>Apellido</h4>
+      <TextField {...lastName} />
+      <h4>Ancho</h4>
+      <span>{width}</span>
     </Box>
   );
 }
